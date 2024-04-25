74a086ed41cc9426d46dbd9755cac944ea6cc77f
sidak.chuchra <sidak.chuchra@kochartech.com>
2024-04-26 00:27:11 +0530

    axios

--- client/src/config/axios.config.js
+++ client/src/config/axios.config.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance with default configuration
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/api', // Base URL for API requests
+  baseURL: 'https://xube.onrender.com/api', // Base URL for API requests
   headers: {
     'Content-Type': 'application/json', // Common header for JSON requests
   },
